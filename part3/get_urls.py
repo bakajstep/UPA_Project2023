6d31be2198834cbce33dcb371ceb8fb0d8edef14
sbakaj <stepan.bakaj@gmail.com>
2023-11-28 15:12:07 +0100

    Print data of each product

--- part3/get_urls.py
+++ part3/get_urls.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 base_url = 'https://www.tiledepot.co.nz/browse/tiles/wall-tiles/'
-all_urls = []
+# all_urls = []
 
 # Iterate over all pages(1-3)
 for page_number in range(1, 4):
@@ -24,14 +24,15 @@ for page_number in range(1, 4):
             # Find <a> tag and get 'href' attribute
             a_tag = product.find('a')
             if a_tag and a_tag.has_attr('href'):
-                all_urls.append(a_tag['href'])
+                print(a_tag['href'])
+                # all_urls.append(a_tag['href'])
     else:
         print(f"Failed to retrieve page {page_number}")
 
-# Store URLs to text file
-with open('urls.txt', 'w') as file:
-    for i, url in enumerate(all_urls):
-        file.write(url)
-        # Check if it's not the last URL to avoid adding a newline
-        if i < len(all_urls) - 1:
-            file.write('\n')
+# # Store URLs to text file
+# with open('urls.txt', 'w') as file:
+#     for i, url in enumerate(all_urls):
+#         file.write(url)
+#         # Check if it's not the last URL to avoid adding a newline
+#         if i < len(all_urls) - 1:
+#             file.write('\n')
